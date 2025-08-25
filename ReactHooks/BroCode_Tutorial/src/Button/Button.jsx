616b1c24d1208bdb089f659466c4f36927a9f4be
AbhayPatil-005 <abhay.official005@gmail.com>
2025-08-25 12:37:33 +0530

    BroCode Tut: Inline Styling css in react

--- ReactHooks/BroCode_Tutorial/src/Button/Button.jsx
+++ ReactHooks/BroCode_Tutorial/src/Button/Button.jsx
@@ -1,8 +1,16 @@
-import styles from './Button.module.css';
-// if we want to use css module then we have to import it like this and use it like className={styles.button}
+// Now we will see how to use inline css in react.
+
 const Button = () => {
-    return <button className={styles.button}>Click Me</button>;
+    const styles ={
+        backgroundColor : "hsl(225, 100%, 65%)",
+        color : "white",
+        padding : "10px 20px",
+        border : "none",
+        borderRadius :" 5px",
+        cursor : "pointer",
+    }
+
+    return <button style={styles}>Click Me</button>;
     }
 export default Button;
 
-// This how we use css module in react
