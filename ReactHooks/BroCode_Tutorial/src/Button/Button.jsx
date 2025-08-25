c039e08547c788f7f493b956ef0c10b8a434ab4c
AbhayPatil-005 <abhay.official005@gmail.com>
2025-08-25 12:39:16 +0530

    BroCode Tut: Inline Styling css in react, ADDED COMMENTS FOR QUICK REVISION

--- ReactHooks/BroCode_Tutorial/src/Button/Button.jsx
+++ ReactHooks/BroCode_Tutorial/src/Button/Button.jsx
@@ -14,3 +14,12 @@ const Button = () => {
     }
 export default Button;
 
+// This inline styling is not recommended for large projects 
+// but it is good for small projects or for quick prototyping.
+
+/*
+* Lets see syntaxes of css in inline, html, and its own css syntax
+* Inline css using js -> backgroundColor : "hsl(225, 100%, 65%)", (like js object)
+* Html css -> background-color : "hsl(225, 100%, 65%)" (each attribute is separated by semicolon)
+* Css file : background-color : hsl(225, 100%, 65%);
+ */
