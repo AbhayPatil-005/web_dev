bf30501f82a46c96cccc8b481dcf58f7af8504e8
AbhayPatil-005 <abhay.official005@gmail.com>
2025-12-01 21:38:16 +0530

    chore: Added logo image in header bar. stored image in public folder.

--- learning_next_js/my-app/app/components/Header.jsx
+++ learning_next_js/my-app/app/components/Header.jsx
@@ -1,11 +1,13 @@
-
+import Image from "next/image";
 
 const Header=()=>{
     return(<header>
 
         
-        <nav style={{ display:"flex", justifyContent:"space-evenly", padding: 16, background: "#f3f3f3" }}>
-            <h1>My store header</h1> 
+        <nav style={{ display:"flex", gap:"50px", padding: 16, background: "var(--background)" }}>
+            
+                <Image src={'/image2.png'} height={10} width={20} alt="logo" />
+                <h1>My store header</h1> 
             <a href="/" style={{ marginRight: 12 }}>Home</a>
             <a href="/products">Products</a>
         </nav>
